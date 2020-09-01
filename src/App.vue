565ff1ff65dc0903306e7503ad1ab8b8b79f3f9e
cheeseq <dr-cheese@ya.ru>
2020-09-01 21:24:22 +0700

    vue cli init and basic sketch

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,88 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div style="padding-top: 2rem;">
+      <div class="row">
+        <div class="col s12">
+          <button class="btn" v-on:click="$modal.show('add-car-modal')">Добавить авто</button>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col s12">
+          <h3>Зарегистрированные авто</h3>
+          <table class="centered highlight">
+            <thead>
+            <tr>
+              <th>Марка</th>
+              <th>Гос.номер</th>
+              <th>Ф.И.О</th>
+              <th>Телефон</th>
+              <th>Заезд</th>
+              <th>Истекает</th>
+              <th>Статус</th>
+              <th></th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="car of cars" v-bind:key="car.owner_phone">
+              <td>{{ car.manufacturer }} {{ car.model }}</td>
+              <td>{{ car.gov_id }}</td>
+              <td>{{ car.owner_fullname }}</td>
+              <td>{{ car.owner_phone }}</td>
+              <td>{{ car.registered_at|date }}</td>
+              <td>{{ car.expires_at || '-' }}</td>
+              <td>{{ car.status }}</td>
+              <td>
+                <a href="#" class="btn dropdown-trigger" data-target="ex1">Действия</a>
+                <ul id='ex1' class='dropdown-content'>
+                  <li><a class="modal-trigger" href="#details-modal">Подробно</a></li>
+                  <li><a href="#!" class="green-text lighten-2-text">Расчет</a></li>
+                </ul>
+              </td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+    <modal name="add-car-modal" :min-width="200"
+           :min-height="200"
+           :scrollable="true"
+           :reset="true"
+           width="60%"
+           height="auto">
+      <add-car v-on:car-created="pushCar"></add-car>
+    </modal>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "materialize-css/dist/css/materialize.min.css";
+import AddCar from "@/components/AddCar";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {AddCar},
+  data() {
+    return {
+      cars: [
+        {
+          "manufacturer": "Hyundai",
+          "model": "Solaris",
+          "gov_id": "АА111А",
+          "owner_fullname": "Иванов Иван Иванович",
+          "owner_phone": "+7 999 999 99 99",
+          "registered_at": new Date(2020, 8, 25, 11, 1).toLocaleString(),
+          "expires_at": null,
+          "status": "Не оплачен"
+        }
+      ]
+    }
+  },
+  methods: {
+    pushCar(car) {
+      //@todo send to api
+      this.cars.push(car);
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/AddCar.vue
@@ -0,0 +1,116 @@
+<template>
+  <div>
+    <form style="padding: 2rem;">
+      <div class="row valign-wrapper">
+        <div class="col s3">
+          <label>
+            <input type="checkbox" v-model="car.is_prepay">
+            <span>Предоплата</span>
+          </label>
+        </div>
+        <div class="col s3">
+          <label id="prepay_sum" v-show="car.is_prepay">
+            Сумма
+            <input type="text" v-model="car.prepay_sum">
+          </label>
+        </div>
+        <div class="col s3">
+          <label id="prepay_deadline" v-show="car.is_prepay">
+            Дата истечения
+            <date-picker v-model="car.prepay_expires_at"></date-picker>
+          </label>
+        </div>
+        <div class="col s3">
+          <label v-show="car.is_prepay">
+            Время истечения
+            <date-picker type="time" format="hh:mm" v-model="car.prepay_expires_at_time"></date-picker>
+          </label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col s4">
+          <label for="auto">Марка</label>
+          <input type="text" id="auto" v-model="car.manufacturer">
+        </div>
+        <div class="col s4">
+          <label for="auto">Модель</label>
+          <input type="text" v-model="car.model">
+        </div>
+        <div class="col s4">
+          <label for="nomer">Гос.номер</label>
+          <input type="text" id="nomer" v-model="car.gov_id">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col s8">
+          <label for="fio">Ф.И.О. владельца</label>
+          <input type="text" id="fio" v-model="car.owner_fullname">
+        </div>
+        <div class="col s4">
+          <label for="phone">Телефон</label>
+          <input type="text" id="phone" v-model="car.owner_phone">
+        </div>
+        <div class="row">
+          <div class="col s12">
+            <label for="note">Комментарий</label>
+            <textarea id="note" class="materialize-textarea" v-model="car.note"></textarea>
+          </div>
+        </div>
+      </div>
+    </form>
+    <div class="row">
+      <div class="col s5 offset-s7">
+        <div class="row">
+          <div class="col s5">
+            <button class="btn red lighten-2" v-on:click="$modal.hide('add-car-modal')">Отмена</button>
+          </div>
+          <div class="col s7">
+            <button class="btn green lighten-2" v-on:click="addCar">Сохранить</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import DatePicker from 'vue2-datepicker';
+import 'vue2-datepicker/index.css';
+import 'vue2-datepicker/locale/ru'
+
+export default {
+  name: "AddCar",
+  components: {
+    DatePicker
+  },
+  data() {
+    return {
+      is_prepay: false,
+      car: {
+        is_prepay: false,
+        prepay_sum: null,
+        prepay_expires_at: new Date(),
+        manufacturer: null,
+        model: null,
+        gov_id: null,
+        owner_fullname: null,
+        owner_phone: null,
+        note: null
+      }
+    }
+  },
+  methods: {
+    addCar() {
+      this.$emit("car-created", this.car);
+      this.$modal.hide("add-car-modal");
+    }
+  }
+
+}
+</script>
+
+<style>
+.mx-datepicker {
+  width: 100%;
+}
+</style>
