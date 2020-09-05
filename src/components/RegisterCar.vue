9be8eb5f854b51466204e74ee46dcd47a1364519
cheeseq <dr-cheese@ya.ru>
2020-09-05 18:25:44 +0700

    added phone mask;

--- src/components/RegisterCar.vue
+++ src/components/RegisterCar.vue
@@ -66,7 +66,7 @@
         </div>
         <div class="col s4">
           <div class="input-field">
-            <input type="text" id="phone" v-model="car.owner_phone" placeholder="Телефон">
+            <input type="text" id="phone" v-model="car.owner_phone" placeholder="Телефон" v-mask="'+7 ### ### ## ##'">
           </div>
         </div>
         <div class="row">
--- src/main.js
+++ src/main.js
@@ -2,11 +2,17 @@ import Vue from 'vue';
 import App from './App.vue';
 import VModal from 'vue-js-modal';
 import Vuelidate from "vuelidate/src";
+import VueMask from 'v-mask';
 
 
 Vue.config.productionTip = false;
 Vue.use(VModal);
 Vue.use(Vuelidate);
+Vue.use(VueMask, {
+  placeholders: {
+    Я: /[\wа-яА-Я]/
+  }
+});
 
 new Vue({
   render: h => h(App),
